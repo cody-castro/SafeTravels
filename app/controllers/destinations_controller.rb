44526c879a45324677ce70edb0071a35d26f5c6e
David-Kirsch <davidjkirsch@gmail.com>
2020-08-17 11:58:14 -0400

    Created show and index page

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,31 +1,14 @@
 class DestinationsController < ApplicationController
 def index
+    @destinations = Destination.all
+end
+
+
+def show
+    @destination = Destination.find(params[:id])
     
 end
 
-def new
-
-end
-
-def create
-
-end
-
-def edit
-
-end
-
-def update
-
-end
-
-def show
-
-end
-
-def destroy
-
-end
 
 
 end
