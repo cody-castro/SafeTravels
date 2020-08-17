a9ee7d62fb1ca366abbbb238d5d960d9c175ddfc
David-Kirsch <davidjkirsch@gmail.com>
2020-08-17 17:36:26 -0400

    Full controllers and views

--- app/controllers/travelers_controller.rb
+++ app/controllers/travelers_controller.rb
@@ -1,14 +1,12 @@
 class TravelersController < ApplicationController
-def index
-    
-end
-
+before_action :find_traveler, only: [:show, :edit, :destroy, :update]
 def new
-
+    @traveler = Traveler.new
 end
 
 def create
-
+    @traveler = Traveler.create(traveler_settings)
+    redirect_to traveler_path(@traveler)
 end
 
 def edit
@@ -16,7 +14,8 @@ def edit
 end
 
 def update
-
+    @traveler.update(traveler_settings)
+    redirect_to traveler_path(@traveler)
 end
 
 def show
@@ -24,7 +23,18 @@ def show
 end
 
 def destroy
-
+    @traveler.destroy
 end
+
+private
+
+def traveler_settings
+    params.require(:traveler).permit(:username, :dob, :password_digest, :about_me)
+end
+
+def find_traveler
+    @traveler = Traveler.find(params[:id])
+end
+
 
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,32 +1,43 @@
 class TripsController < ApplicationController
-
-def index
-    
-end
+before_action :find_trip, only: [:show, :edit, :update, :destroy]
 
 def new
-
+    @trip = Trip.new
+    @travelers = Traveler.all
+    @destinations = Destination.all
 end
 
 def create
-
+    @trip = Trip.create(trip_settings)
+    redirect_to traveler_path(@trip.traveler_id)
 end
 
 def edit
-
+    @travelers = Traveler.all
+    @destinations = Destination.all
 end
 
 def update
-
+    @trip.update(trip_settings)
+    redirect_to traveler_path(@trip.traveler_id)
 end
 
 def show
 
 end
 
-def destroy
 
+def destroy
+    
 end
 
+private
 
+def trip_settings
+    params.require(:trip).permit(:traveler_id, :destination_id, :name)
+end
+
+def find_trip
+    @trip = Trip.find(params[:id])
+end
 end
